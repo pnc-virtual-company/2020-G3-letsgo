aac2aabe801f7c8de6b74f91fabe13bd5f9ce103
darom90 <darom.chhuon@student.passerellesnumeriques.org>
2020-07-15 11:16:05 +0700

    #38 edit_profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,7 +108,7 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Profile</a>
+                                <a class="dropdown-item" href="{{route('user.edit',Auth::user()->id)}}">Profile</a>
                                 
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
