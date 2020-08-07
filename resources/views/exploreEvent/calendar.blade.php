cbe7ddf360e842d6150fc71786d87da60e106413
piseth.poeurn <piseth.poeurn@student.passerellesnumeriques.org>
2020-08-07 09:34:55 +0700

    #88 detail event in calendar

--- resources/views/exploreEvent/calendar.blade.php
+++ resources/views/exploreEvent/calendar.blade.php
@@ -56,6 +56,28 @@
     <div class="row">
         <div class="col-12">
             <div id="calendar"></div>
+            {{-- <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button> --}}
+
+              <!-- Modal -->
+              <div id="myModal" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+
+                  <!-- Modal content-->
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                      <h4 class="modal-title">Modal Header</h4>
+                    </div>
+                    <div class="modal-body">
+                      <p>Some text in the modal.</p>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
         </div>
     </div>
 </div>
@@ -75,10 +97,15 @@
         },
       @endforeach
     ] ,
+        
     editable: true,
     selectable: true
-  });
+// eventClick: function(event){
+//           $("#myModal").modal("show");
+//           $("#myModal .modal-body p").text(event.title);
 
+//         }
+  });
   calendar.render();
 });
 </script>
